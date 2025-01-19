09c10cbf8212bd6c24063a65a1b1d1349d5d8b53
Chirasthi Thennakoon <119811942+ChirasthiT@users.noreply.github.com>
2025-01-19 21:20:10 +0530

    Flask template bug fix

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from Location_Identification.li import li_blueprint
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb+srv://admin:<admindsgp66>@dsgp.e5yrm.mongodb.net/?retryWrites=true&w=majority&appName=DSGP'
+
+# MongoDB Initialization
+mongo = PyMongo(app)
+app.register_blueprint(li_blueprint, url_prefix='/location-identification')
+li_blueprint.mongo = mongo
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/li')
+def feature1():
+    return render_template('li.html')
+
+
+# @app.route('/feature2')
+# def feature2():
+#     return "Feature 2 Page"
+
+
+# @app.route('/feature3')
+# def feature3():
+#     return "Feature 3 Page"
+
+
+# @app.route('/feature4')
+# def feature4():
+#     return "Feature 4 Page"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
