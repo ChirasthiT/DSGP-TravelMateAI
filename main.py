aaa6a90d709f88b314b3870fbb8bd9571a111851
Chirasthi Thennakoon <119811942+ChirasthiT@users.noreply.github.com>
2025-02-24 10:25:36 +0530

    User management Sign-in and Logins validation and Database referencing

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
 from Location_Identification.li import li_blueprint
+from Frontend.frontend import frontend_blueprint
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.register_blueprint(li_blueprint, url_prefix='/location-identification')
+app.register_blueprint(frontend_blueprint, url_prefix='/auth')
 
 client = MongoClient('mongodb+srv://admin:admindsgp66@dsgp.e5yrm.mongodb.net/')
 db = client['travelmateai']
 
 li_blueprint.db = db
+frontend_blueprint.db = db
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('landing.html')
 
 
 @app.route('/li.home')
