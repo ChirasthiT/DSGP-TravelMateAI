880674346025bd4ec40930c14097a741bf2bc069
meth-nin <meth2468nin@gmail.com>
2025-03-07 20:22:43 +0530

    Basic Flask API setup and MongoDB connection to store Sos Alerts

--- Em-Helper/API/database.py
+++ Em-Helper/API/database.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "travelmateai"
 
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
+client = MongoClient('mongodb+srv://admin:admindsgp66@dsgp.e5yrm.mongodb.net/')
+db = client['travelmateai']
 collection = db["sos_alerts"]
 
 def save_sos_alert(alert_data):
